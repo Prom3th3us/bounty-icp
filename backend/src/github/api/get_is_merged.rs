cd5fcd6e9dd74689bc39eb12813cad7226b39899
Franco Testagrossa <franco.testagrossa@gmail.com>
2024-04-27 18:04:26 +0200

    Finish to resolve #3

--- backend/src/github/api/get_is_merged.rs
+++ backend/src/github/api/get_is_merged.rs
@@ -28,14 +28,7 @@ pub async fn get_is_merged_impl(owner: String, repo: String, pr_nbr: i32) -> Str
     // Make the HTTP request and wait for the response
     match http_request(request).await {
         Ok((response,)) => {
-            //We need to decode that Vec<u8> that is the body into readable text.
-            //To do this, we:
-            //  1. Call `String::from_utf8()` on response.body
-            //  3. We use a switch to explicitly call out both cases of decoding the Blob into ?Text
-            let str_body = String::from_utf8(response.body)
-                .expect("Transformed response is not UTF-8 encoded.");
-
-            return str_body;
+            return response.status.to_string();
         }
         Err((rejection_code, message)) => {
             panic!(
